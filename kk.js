67a56e7d5a3c4f73232bfa72899b3bf0b4e989e0
icw82 <icw82@yandex.ru>
2016-01-09 17:45:22 +0500

    Array Buffer in Each

--- kk.js
+++ kk.js
@@ -54,7 +54,6 @@ if (typeof module == kenzo._o && typeof module.exports == kenzo._o) {
 
 kenzo.r = root;
 
-// end ————————————————————————————————————————————————————————————————————————————————————— 100 ——|
 }());
 
 // Перебор массива
@@ -71,11 +70,12 @@ kk.each = function(array, callback) {
         index;
 
 //    console.log('**', array instanceof MutationRecord);
-
     if (typeof array === kenzo._s && kenzo.d)
         array = document.querySelectorAll(array);
     else if (typeof array === kenzo._n)
-        array = Array(Math.floor(array));
+        array = Array(Math.floor(array))
+    else if (ArrayBuffer.isView(array) && (array.lenght > 0))
+        array = Array.prototype.slice.call(array);
 
     if (typeof args[2] == kenzo._f) {
         def = args[2];
