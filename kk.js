33440c7c98d3ff4cea49a7c61a12563ee9d1463e
icw82 <icw82@yandex.ru>
2016-08-17 20:09:55 +0500

    update

--- kk.js
+++ kk.js
@@ -257,6 +257,8 @@ kk.class_forever = function(class_name, element){
 kk.Event = function(key) {
     var kenzo = kk;
     var listeners = [];
+    var is_completed = false;
+    var last_data;
 
     Object.defineProperty(this, 'key', {
         get: function() {return key}
@@ -272,7 +274,11 @@ kk.Event = function(key) {
         if (!kenzo.is_f(listener) || this.hasListener(listener))
             return;
 
-        listeners.push(listener);
+        if (this.is_completed) {
+            listener(last_data);
+        } else {
+            listeners.push(listener);
+        }
     }
 
     this.removeListener = function(listener) {
@@ -286,6 +292,9 @@ kk.Event = function(key) {
 
     // Если ключ задан, то он передаётся первым аргументом.
     this.dispatch = function() {
+        if (this.is_completed)
+            return;
+
         var args = arguments;
         var data;
 
@@ -299,9 +308,22 @@ kk.Event = function(key) {
             }
         }
 
+        last_data = data;
+
         kk.each (listeners, function(listener) {
             listener(data);
         });
+
+        return true;
+    }
+
+    this.complete = function() {
+        if (this.is_completed)
+            return;
+
+        if (this.dispatch.apply(this, arguments)) {
+            this.is_completed = true;
+        }
     }
 };
 
